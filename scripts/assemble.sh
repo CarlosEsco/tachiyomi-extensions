8e3fcd91a743ecabc07152dbf783f3e2a094af54
Carlos Escobedo <cargo8005@gmail.com>
2017-11-29 15:29:34 -0500

    test apk

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -1,10 +1,10 @@
 set -ev
 if [ "${COMPONENT}" = "all-ehentai" ]; then
 	./gradlew clean assembleDebug -a -b src/all/ehentai/build.gradle
-	cp -R src/all/ehentai/build/outputs/apk/debug/. /apk/
+	cp -R src/all/ehentai/build/outputs/apk/debug/. apk/
 elif [ "${COMPONENT}" = "all-nhentai" ]; then
 	./gradlew clean assembleDebug -a -b src/all/nhentai/build.gradle
-	cp -R src/all/nhentai/build/outputs/apk/debug/. /apk/
+	cp -R src/all/nhentai/build/outputs/apk/debug/. apk/
 else
 	echo "module doesn't exist"
 	exit 1
