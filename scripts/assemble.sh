32e67b9b45d4d72f632d22d16d59c2fc37ec6efa
Carlos Escobedo <cargo8005@gmail.com>
2017-11-29 13:16:15 -0500

    fix assemble.sh

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -1,7 +1,7 @@
 set -ev
 
 if [ "${COMPONENT}" = "all-ehentai" ]; then
-	./gradlew clean assembleDebug -p all-ehentai
+	./gradlew clean assembleDebug -p src\all\ehentai
 else
 	echo "module doesn't exist"
 	exit 1
